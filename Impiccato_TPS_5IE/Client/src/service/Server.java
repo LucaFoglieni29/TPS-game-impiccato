0c6d9ce30c305f40789698532724ffab18464b88
LucaFoglieni29 <97253811+LucaFoglieni29@users.noreply.github.com>
2022-12-19 19:25:54 +0100

    Update Server.java

--- Impiccato_TPS_5IE/Client/src/service/Server.java
+++ Impiccato_TPS_5IE/Client/src/service/Server.java
@@ -1,7 +1,7 @@
 package service;
-public class Server {
+public class Server {           //Questa è una classe che rappresenta un server. Ha tre campi: srvName, srvIP, e port. 
     private String srvName;
-    private String srvIP;
+    private String srvIP;               //Questi campi contengono rispettivamente il nome del server, l'indirizzo IP del server e il numero di porta su cui il server è in ascolto. La classe ha metodi getter e setter per ciascuno di questi campi.
     private String port;
 
     public String getSrvName() {
@@ -31,7 +31,7 @@ public class Server {
     public Server() {
     }
 
-    public Server(String srvName, String srvIP, String port) {
+    public Server(String srvName, String srvIP, String port) {          //Dispone inoltre di un costruttore predefinito e di un costruttore che accetta tre argomenti: srvName, srvIPe port, utilizzati per inizializzare i campi.
         this.srvName = srvName;
         this.srvIP = srvIP;
         this.port = port;
