4491ccb36687f4052f83f83d8dd82046c4be46a0
LucaFoglieni29 <97253811+LucaFoglieni29@users.noreply.github.com>
2022-12-20 19:32:36 +0100

    Update HangmanServer.java

--- Impiccato_TPS_5IE/Server/src/HangmanServer.java
+++ Impiccato_TPS_5IE/Server/src/HangmanServer.java
@@ -5,25 +5,23 @@ import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class HangmanServer {
+public class HangmanServer {        //classe, che contiene un metodo principale che è il punto di ingresso per l'applicazione server.
     private static ArrayList<String> words = null;
     private static boolean listening = true;
-
-    private static final int availableThreads = Runtime.getRuntime().availableProcessors();
-    private static ServerSocket serverSocket = null;
-    private static ExecutorService executorService = Executors.newFixedThreadPool(availableThreads);
-
+    private static ServerSocket serverSocket = null;        //Il readWordsmetodo legge un elenco di parole da un file e le memorizza wordsnell'elenco. Questo elenco verrà utilizzato per giocare al gioco dell'impiccato con i clienti. Il listenmetodo crea un ServerSocketoggetto e ascolta le connessioni client in entrata sulla porta specificata
+    
+    
     public static void main(String[] args) {
         readWords();
         listen(args);
     }
 
-    private static void listen(String[] args) {
-        try {
+    private static void listen(String[] args) {         //oggetto e ascolta le connessioni client in entrata sulla porta specificata. Quando viene stabilita una connessione, crea un nuovo HangmanServerHandleroggetto e gli passa l'elenco 
+        try {                                           //di parole e il socket del client, e lo invia a ExecutorServiceun'esecuzione in concomitanza con altre richieste del client. La HangmanServerHandlerclasse è responsabile della gestione della comunicazione con il cliente e del gioco dell'impiccato.
             serverSocket = new ServerSocket(setPort(args));
             System.out.printf("Discussioni disponibili: [%s]%n", availableThreads);
             System.out.printf("In attesa di connessioni dalla porta [%s]...%n%n", serverSocket.getLocalPort());
-            while (listening) {
+            while (listening) {     //variabile è un flag che controlla il ciclo che ascolta le connessioni client. Finché lo è true, il server continuerà ad accettare nuove connessioni. Se è impostato su false, il ciclo terminerà e il server smetterà di ascoltare nuove connessioni.
                 Socket clientSocket = serverSocket.accept();
                 executorService.execute(new HangmanServerHandler(words, clientSocket));
                 
@@ -34,7 +32,7 @@ public class HangmanServer {
         }
     }
 
-    private static int setPort(String[] args) {
+    private static int setPort(String[] args) {         //setta la porta 5151
         int port = 5151;
         if (args != null && args.length > 0) {
             try {
@@ -46,8 +44,8 @@ public class HangmanServer {
         return port;
     }
 
-    private static void readWords() {
-        words = new ArrayList<String>();
+    private static void readWords() {       //legge un elenco di parole da un file chiamato words.txte le memorizza in un file ArrayListchiamato words.
+        words = new ArrayList<String>();    //Prima crea un vuoto ArrayListchiamato words. Quindi, crea un BufferedReader oggetto chiamato bReader che viene utilizzato per leggere un file chiamato words.txt.
         String line;
         try {
             BufferedReader bReader = new BufferedReader(new FileReader("Server/words.txt"));
@@ -64,12 +62,13 @@ public class HangmanServer {
         }
     }
 
-    private void stopServer() {
-        //finire esecuzione servizio
+    private void stopServer() {         
+        //Questo codice definisce un metodo chiamato stopServerche viene utilizzato per arrestare il server. Il metodo imposta la listening variabile su false per indicare che il server non è più in attesa di connessioni in entrata.
+        
         listening = false;
         executorService.shutdownNow();
         try {
-            //finire di accettare richieste
+         
             serverSocket.close();
         } catch (IOException e) {
             System.out.println("Errore nel server,chiusura in corso");
